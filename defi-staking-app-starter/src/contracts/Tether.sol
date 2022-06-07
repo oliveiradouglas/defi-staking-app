5d17869619c1c9e7f9587bb57afbdb76be5fec94
Douglas Oliveira <oliveirandouglas@gmail.com>
2022-06-07 10:38:43 -0300

    first commit

---
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract Tether {
+    string public name = "Tether Douglas";
+    string public symbol = "mUSDT";
+    uint256 public totalSupply = 1000000000000000000;
+    uint8 public decimals = 18;
+}
